299fd710f2275ab446af3e2fd8e0bfdaee584728
JorgeBel <jorgeluizbelodelima@gmail.com>
2024-03-10 03:27:01 -0300

    first commit

---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("João")); // Saída: Olá João
